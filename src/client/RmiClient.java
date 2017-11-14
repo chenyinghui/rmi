3cf643205f60de457e78f858a713b730dfcb90a3
Administrator <Administrator@QH-20161224FFIG>
2017-11-14 10:55:32 +0800

    test send message

--- src/client/RmiClient.java
+++ src/client/RmiClient.java
@@ -7,8 +7,9 @@ import rmi.RmiServerIntf;
 public class RmiClient { 
     public static void main(String args[]) throws Exception {
         RmiServerIntf obj = (RmiServerIntf)Naming.lookup("//localhost:1099/RmiServer");
-        for(int i = 0; i < 20; i ++){
-        	System.out.println(obj.getMessage()); 
-        }
+//        for(int i = 0; i < 20; i ++){
+//        	System.out.println(obj.getMessage()); 
+//        }
+        System.err.println(obj.increaseAndGet(20));
     }
 }
